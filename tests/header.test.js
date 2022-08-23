59ed8d337346323a04443a0844c2b2927b96081b
mbukeRepo <mbuke404@gmail.com>
2022-08-23 12:56:01 +0200

    simulated login in my test suite

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,5 @@
 const puppeteer = require("puppeteer");
+const Keygrip = require("keygrip");
 
 let browser;
 let page;
@@ -12,16 +13,31 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-   await browser.close();
+   //await browser.close();
 });
 
 test("header logo has correct text", async() => {
       	const text = await page.$eval("a.brand-logo", el => el.innerHTML);
 	expect(text).toEqual("Blogster");
-}, 70000);
+}, 90000);
 
 test("clicking login starts oauth flow", async () => {
   await page.click('.right a');
   const url = await page.url();
   expect(url).toMatch(/accounts\.google\.com/);
-}, 70000);
+}, 90000);
+
+test.only("when signed in show logout button", async () => {
+  // get userID and generate face session
+  const user ="62ff77abc269b88fa053a2cd";
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {passport: {user}};
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString('base64');
+  const keygrip = new Keygrip(["helloworld"]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  // setting session and signature on our page instance
+  await page.setCookie({name: 'session', value: sessionString});
+  await page.setCookie({name: 'session.sig', value: sig});
+  await page.goto('localhost:5000');
+ }, 90000);
