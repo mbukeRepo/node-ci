7e07d11197660e46e22d116f44f32aba1e982473
mbukeRepo <mbuke404@gmail.com>
2022-08-24 14:22:50 +0200

    restructed the flow of tests and added more tests

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -10,11 +10,35 @@ afterEach(async () => {
   await page.close();
 });
 
-test("when logged in, can see blog create form", async () => {
+
+describe("when not logged in", () => {
+
+});
+
+describe("when logged in", () => {
+  beforeEach(async () => {
     await page.login();
     await page.waitFor('a.btn-floating');
     await page.click('a.btn-floating');
+  });
+  test("can see blog create form", async () => {
     await page.waitFor('form label');
     const label = await page.getContent("form label");
     expect(label).toEqual("Blog Title");
+  });
+
+  describe("and when using invalid inputs", () => {
+    beforeEach(async () => {
+       await page.click('form button');
+    });
+    test("the form shows error message", async () => {
+      const titleError = await page.getContent(".title .red-text");
+      const contentError = await page.getContent(".content .red-text");
+      const msg = "You must provide a value";
+      expect(titleError).toEqual(msg);
+      expect(contentError).toEqual(msg);
+    });
+  });
+
+  describe("and when using valid form inputs", () => {});
 });
