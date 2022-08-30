81fc5eaebf6d5bcffaa52205b29be57890d74910
mbukeRepo <mbuke404@gmail.com>
2022-08-30 12:23:13 +0200

    full integrated feature of uploading to aws s3

--- client/src/components/blogs/BlogShow.js
+++ client/src/components/blogs/BlogShow.js
@@ -6,6 +6,11 @@ class BlogShow extends Component {
   componentDidMount() {
     this.props.fetchBlog(this.props.match.params._id);
   }
+  renderImage(){
+   if(this.props.blog.imageUrl){
+     return <img src={"http://046cb39f13468099.s3.ap-south-1.amazonaws.com/" + this.props.blog.imageUrl}/>
+   }
+  }
 
   render() {
     if (!this.props.blog) {
@@ -18,6 +23,7 @@ class BlogShow extends Component {
       <div>
         <h3>{title}</h3>
         <p>{content}</p>
+        {this.renderImage()}
       </div>
     );
   }
